17e977fab106a1463456f17e3d97d17585afffcf
Ashishxo <dhiwarashish4@gmail.com>
2024-07-02 20:22:58 +0530

    mongodb connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hehe")
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env"
+})
+
+const PORT = process.env.PORT || 3000
+
+
+
+connectDB()
+.then(() => {
+    app.listen(PORT, ()=>{
+        console.log(`Listening at Port: ${PORT}`);
+    }) 
+})
+.catch((err) => {
+    console.log("MongoDB Connection Error: ", err)
+})
